9505c1605e6e4e16fcaeb7433f7d641d9f4b3604
Ante Sepic <antesepic@protonmail.com>
2021-09-09 17:53:31 +0200

    :sparkles: Initial app version

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -1,16 +1,95 @@
-import React from 'react';
-import { Paragraph } from '@contentful/forma-36-react-components';
-import { FieldExtensionSDK } from '@contentful/app-sdk';
+import React, { useEffect, useMemo, useState } from "react";
+import { Paragraph } from "@contentful/forma-36-react-components";
+import { FieldAPI, FieldExtensionSDK } from "@contentful/app-sdk";
+import { Field } from "@contentful/default-field-editors";
+import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
 
 interface FieldProps {
   sdk: FieldExtensionSDK;
 }
 
-const Field = (props: FieldProps) => {
-  // If you only want to extend Contentful's default editing experience
-  // reuse Contentful's editor components
-  // -> https://www.contentful.com/developers/docs/extensibility/field-editors/
-  return <Paragraph>Hello Entry Field Component</Paragraph>;
+type ComponentType = "web" | "mobile" | undefined;
+
+const CustomField = (props: FieldProps) => {
+  const { sdk } = props;
+
+  // Auto resize app iframe
+  useEffect(() => {
+    sdk.window.startAutoResizer();
+
+    return () => {
+      sdk.window.stopAutoResizer();
+    };
+  }, [sdk.window]);
+
+  const booleanToComponentType = (
+    componentTypeBool: boolean | undefined
+  ): ComponentType => {
+    return componentTypeBool === undefined
+      ? undefined
+      : componentTypeBool === true
+      ? "web"
+      : "mobile";
+  };
+
+  const [componentType, setComponentType] = useState<ComponentType>(
+    booleanToComponentType(sdk.field.getValue())
+  );
+
+  useEffect(() => {
+    sdk.field.onValueChanged((newValue) => {
+      setComponentType(booleanToComponentType(newValue));
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  // Screenshot
+  const screenshotField = sdk.entry.fields.screenshot;
+
+  (screenshotField as unknown as FieldAPI).locale = sdk.locales.default;
+  (screenshotField as unknown as FieldAPI).setInvalid = () => false;
+  (screenshotField as unknown as FieldAPI).onSchemaErrorsChanged =
+    () => () => {};
+
+  // Code editor
+  const codeSnippetValue = useMemo(() => {
+    return sdk.entry.fields.codeSnippet.getValue();
+  }, [componentType]);
+
+  return (
+    <div className="f36-padding-bottom--4xl">
+      <Field sdk={sdk} widgetId="boolean" />
+
+      <div className="f36-padding-top--l">
+        {componentType === undefined ? (
+          <Paragraph>Please choose the component type</Paragraph>
+        ) : componentType === "mobile" ? (
+          <Field
+            sdk={{
+              ...sdk,
+              field: screenshotField as unknown as FieldAPI,
+            }}
+            widgetId={
+              sdk.editor.editorInterface.controls?.find(
+                (control) => control.fieldId === "screenshot"
+              )?.widgetId ?? "assetLinkEditor"
+            }
+          />
+        ) : (
+          <CodeMirror
+            value={codeSnippetValue}
+            height="200px"
+            // @ts-ignore
+            extensions={[javascript({ jsx: true })]}
+            onChange={(value) => {
+              sdk.entry.fields.codeSnippet.setValue(value);
+            }}
+          />
+        )}
+      </div>
+    </div>
+  );
 };
 
-export default Field;
+export default CustomField;
